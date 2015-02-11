1863c630a76c56f2df1e1d5a9a2467003be42e18
jakubwojciech <jakub.wojciech@gmail.com>
2015-02-11 23:59:08 +0000

    Initial commit

---
+++ FloodFill/FloodFill.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloodFill
+{
+    class FloodFill
+    {
+        int width;
+        int height;
+        string image;
+        char[,] map;
+
+        public FloodFill(int width, int height, string image)
+        {
+            this.width = width;
+            this.height = height;
+            this.image = image;
+            fillMap(width, height, image);
+        }
+
+        private void fillMap(int width, int height, string image)
+        {
+            this.map = new char[height, width];
+            int charCouter = 0;
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    map[i, j] = image[charCouter];
+                    charCouter++;
+                }
+            }
+        }
+
+        public string Fill(int x, int y, char character)
+        {
+            char floorChar = map[x, y];
+
+            checkNeighboursAndFill(x, y, floorChar, character);
+
+            return convertMapToString();
+        }
+
+        private string convertMapToString()
+        {
+            string result = "";
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    result += map[i, j];
+                }
+            }
+            return result;
+        }
+
+        private void checkNeighboursAndFill(int x, int y, char floorChar, char filling)
+        {
+            if (map[x, y] == floorChar)
+            {
+                map[x, y] = filling;
+            }
+
+            if (x < width - 1 && map[x + 1, y] == floorChar)
+            {
+                map[x + 1, y] = filling;
+                checkNeighboursAndFill(x + 1, y, floorChar, filling);
+            }
+
+            if (x > 0 && map[x - 1, y] == floorChar)
+            {
+                map[x - 1, y] = filling;
+                checkNeighboursAndFill(x - 1, y, floorChar, filling);
+            }
+
+            if (y < height - 1 && map[x, y + 1] == floorChar)
+            {
+                map[x, y + 1] = filling;
+                checkNeighboursAndFill(x, y + 1, floorChar, filling);
+            }
+
+            if (y > 0 && map[x, y - 1] == floorChar)
+            {
+                map[x, y - 1] = filling;
+                checkNeighboursAndFill(x, y - 1, floorChar, filling);
+            }
+
+        }
+
+    }
+}
---
+++ FloodFill/Tests.cs
@@ -0,0 +1,121 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloodFill
+{
+    [TestFixture]
+    class Tests
+    {
+        [Test]
+        public void Flood_Given3x3Image_FillCorretly()
+        {
+            string image = "+++" +
+                            "+-+" +
+                            "+++";
+            string expectedImage = "+++" +
+                                    "+@+" +
+                                    "+++";
+            FloodFill floodFill = new FloodFill(3, 3, image);
+            string result = floodFill.Fill(1, 1, '@');
+            Assert.That(result, Is.EqualTo(expectedImage));
+        }
+
+        [Test]
+        public void Flood_Given4x4Image_FillCorretly()
+        {
+            string image = "++++" +
+                            "+--+" +
+                            "++++";
+            string expectedImage = "++++" +
+                                    "+@@+" +
+                                    "++++";
+            FloodFill floodFill = new FloodFill(4, 3, image);
+            string result = floodFill.Fill(1, 1, '@');
+            Assert.That(result, Is.EqualTo(expectedImage));
+        }
+
+        [Test]
+        public void Flood_GivenBigImage_FillCorrectly()
+        {
+            string image = 
+            "----------------" +
+            "-++++++++++++++-" +
+            "-+------------+-" +
+            "-++++++++++++-+-" +
+            "-+------------+-" +
+            "-+-++++++++++++-" +
+            "-+------------+-" +
+            "-++++++++++++-+-" +
+            "-+------------+-" +
+            "-+-++++++++++++-" +
+            "-+------------+-" +
+            "-++++++++++++++-" +
+            "-+------------+-" +
+            "-++++++++++++++-" +
+            "----------------";
+            FloodFill floodFill = new FloodFill(16, 15, image);
+            string result = floodFill.Fill(2, 2, '@');
+
+            string expectedImage =
+                "----------------" +
+                "-++++++++++++++-" +
+                "-+@@@@@@@@@@@@+-" +
+                "-++++++++++++@+-" +
+                "-+@@@@@@@@@@@@+-" +
+                "-+@++++++++++++-" +
+                "-+@@@@@@@@@@@@+-" +
+                "-++++++++++++@+-" +
+                "-+@@@@@@@@@@@@+-" +
+                "-+@++++++++++++-" +
+                "-+@@@@@@@@@@@@+-" +
+                "-++++++++++++++-" +
+                "-+------------+-" +
+                "-++++++++++++++-" +
+                "----------------";
+
+            Assert.That(result, Is.EqualTo(expectedImage));
+        }
+
+        [Test]
+        public void Flood_GivenBigImageWithDifferentSigns_FillCorrectly()
+        {
+            string image =
+            "aaaaaaaaa" +
+            "aaadefaaa" +
+            "abcdafgha" +
+            "abcdafgha" +
+            "abcdafgha" +
+            "abcdafgha" +
+            "aacdafgaa" +
+            "aaadafaaa" +
+            "aaaaaaaaa";
+
+
+            FloodFill floodFill = new FloodFill(9, 9, image);
+            string result = floodFill.Fill(8, 3, ',');
+
+            string expectedImage =
+                ",,,,,,,,," +
+                ",,,def,,," +
+                ",bcd,fgh," +
+                ",bcd,fgh," +
+                ",bcd,fgh," +
+                ",bcd,fgh," +
+                ",,cd,fg,," +
+                ",,,d,f,,," +
+                ",,,,,,,,,";
+
+            Assert.That(result, Is.EqualTo(expectedImage));
+        }
+
+        
+
+        
+
+    }
+}
